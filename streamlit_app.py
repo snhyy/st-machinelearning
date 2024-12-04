24d980d1456a7a964fc9311d06c523efbaf5b0e0
lenguyentuyetnhi <129526123+snhyy@users.noreply.github.com>
2024-12-04 19:36:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Prediction App')
 
 st.info('Predict Stock App')
+
+with st.expander('Data'):
+  st.write('Raw data')
+  data = pd.read_csv('https://raw.githubusercontent.com/snhyy/data-test/refs/heads/main/online_retail_II.csv')
+  data
